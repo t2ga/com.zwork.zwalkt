21f2d8fe531547935eefc009d5da88482e8abb31
AT <alexey.telitsyn@gmail.com>
2021-05-02 15:57:16 +0300

    [add] step  6: Modeles

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,10 +1,19 @@
-sap.ui.define(["com/zwork/zwalkt/controller/BaseController"], function (Controller) {
+sap.ui.define([
+    "com/zwork/zwalkt/controller/BaseController",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("com.zwork.zwalkt.controller.MainView", {
         onShowHello: function() {
             //show a native JavaScript alert
-            alert("Hello World!");
+            // alert("Hello World!");
+            var greeting = this
+            .getView()
+            .getModel("i18n")
+            .getResourceBundle()
+            .getText("greeting");
+            MessageToast.show(greeting); //("{i18n>greeting}"); //"greeting=Hello World!"
         }
     });
 });
