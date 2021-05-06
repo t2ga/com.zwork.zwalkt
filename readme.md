a936a3676b0f695b6dced51cc0ddbeead8599c78
t2ga <tltsnsub@gmail.com>
2021-05-06 16:15:01 +0300

    [add] step 22-24

--- readme.md
+++ readme.md
@@ -2,6 +2,11 @@
 
 the project of [walkthrough](https://sapui5.hana.ondemand.com/#/topic/3da5f4be63264db99f2e5b04c5e853db) tutorial.
 
+## To study
+Links with information that have to be reviewed!
+1. [List & ObjectListItem](https://experience.sap.com/fiori-design-web/object-list-item/)
+
+
 Insert the purpose of this project and some interesting infos here
 
 > Create your first UI5 App within a few seconds!
--- uimodule/webapp/controller/InvoiceList.controller.js
+++ uimodule/webapp/controller/InvoiceList.controller.js
@@ -1,16 +1,39 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel"
-], function (Controller, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("com.zwork.zwalkt.controller.InvoiceList", {
+		
+		formatter : formatter,
 
 		onInit : function () {
 			var oViewModel = new JSONModel({
-				currency: "EUR"
+				currency: "JPY" //"EUR"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+
+		onFilterInvoices : function (oEvent) {
+			//build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+			}
+
+			//filter binding
+			//If the query is not empty, we add a new filter object to the still empty array of filters. 
+			// However, if the query is empty, we filter the binding with an empty array. 
+			// This makes sure that we see all list elements again.
+			
+			var oList = this.byId("invoiceList"); //list on the view 'InvoiceList'
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		}
 
 	});
